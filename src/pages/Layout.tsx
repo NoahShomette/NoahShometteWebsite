7012f73f0c87b28a2421dadf7f066de11164bba7
Noah <noahshomette@gmail.com>
2023-02-14 12:02:36 -0500

    fixed bee background

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -6,35 +6,13 @@ import styles from "./Layout.module.css";
 import {ButtonSize} from "../utils/options";
 
 const Layout = () => {
-    const [leftPixels, setLeftPixels] = useState(300);
-
-    useEffect(() => {
-        window.addEventListener("resize", function handleWindowResize() {
-            if (window.innerWidth > 800) {
-                setLeftPixels((leftPixels + window.innerWidth) - 2000);
-            } else {
-                setLeftPixels((leftPixels + 200 + window.innerWidth) - 2000);
-            }
-        });
-        return () => window.removeEventListener("resize", function handleWindowResize() {
-            setLeftPixels((leftPixels + window.innerWidth) - 2000);
-        });
-    }, []);
-
-    useEffect(() => {
-        let bee = document.getElementsByClassName(styles.bee)[0];
-
-        if (bee instanceof HTMLElement) {
-            console.log(leftPixels);
-            bee.style.left = leftPixels + "px";
-        }
-    });
-
 
     return (
         <>
-            <div className={styles.bee}>
-                <BeeComponent/>
+            <div className={styles.beeHolder}>
+                <div className={styles.bee}>
+                    <BeeComponent/>
+                </div>
             </div>
             <Navbar/>
             <Outlet/>
--- src/resources/bee_ts.tsx
+++ src/resources/bee_ts.tsx
@@ -1,4 +1,4 @@
-﻿import * as React from "react"
+import * as React from "react"
 
 const BeeComponent = (props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) => (
     <svg
